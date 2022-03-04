8f8428dc928679116a9b152db43ccca30848aa8b
alicalimli <alicalimli76@gmail.com>
2022-03-04 14:50:23 +0300

    implemented button ripple effect

--- scripts/index.js
+++ scripts/index.js
@@ -6,9 +6,27 @@ sideBar.addEventListener("click", function (e) {
 
   if (!btn) return;
 
+  // Removes the active classes to all buttons
+  // except the btn that has been clicked
+
   navBtns.forEach((el) => {
     if (el !== btn) el.classList.remove("active");
   });
+
+  // Creates ripple effect for the buttons
+
+  const x = e.clientX - btn.offsetLeft;
+  const y = e.clientY - btn.offsetTop;
+
+  const ripple = document.createElement("span");
+  ripple.classList.add("btn-ripple");
+  ripple.style.left = x + "px";
+  ripple.style.top = y + "px";
+  btn.appendChild(ripple);
+
+  setTimeout(() => ripple.remove(), 1000);
+
+  // Toggles active class to the buttons
 
   btn.classList.toggle("active");
 });
