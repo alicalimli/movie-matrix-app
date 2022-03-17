3508e794b618c098e219aaae67865c8b87795d5d
alicalimli <alicalimli76@gmail.com>
2022-03-17 08:48:35 +0300

    searchbar moved to sidebar

--- scripts/views/sideBarBtnsView.js
+++ scripts/views/sideBarBtnsView.js
@@ -3,7 +3,7 @@ import DEFAULT_PAGE from "../config.js";
 class SideBarBtnView {
   _parentEl = document.querySelector(".movie-sidebar-nav");
   _navBtns = document.querySelectorAll(".nav-btn");
-  _icons = document.querySelectorAll(".bx");
+  _icons = this._parentEl.querySelectorAll(".bx");
   buttonPage = "home";
 
   addHandlerEvent(handle) {
@@ -16,13 +16,13 @@ class SideBarBtnView {
 
   updateBtn() {
     this._icons.forEach((el) => {
-      const elParent = el.closest(".nav-btn");
+      const elParent = el.closest(".nav-btn") || el.closest(".nav-form-btn");
       // Stops the function when pageType isnt home
-      if (elParent.dataset.page !== "home") return;
+      if (elParent.dataset.page !== "home" || !elParent) return;
 
       // sets active class to home button
-      el.closest(".nav-btn").classList.toggle("active");
-      el.closest(".nav-btn").querySelector(".bx").classList.toggle("active");
+      elParent.classList.toggle("active");
+      elParent.querySelector(".bx").classList.toggle("active");
     });
   }
 
@@ -48,21 +48,43 @@ class SideBarBtnView {
   }
 
   renderActive(event) {
-    const btn = event.target.closest(".nav-btn");
+    const btn =
+      event.target.closest(".nav-btn") || event.target.closest(".nav-form-btn");
     const sidebar = document.querySelector(".movie-sidebar-nav");
     const expandSideBtn = document.querySelector(".sidebar-expand-btn");
     const overlay = document.querySelector(".overlay-main");
     const mainMovieSection = document.querySelector(".movie-main");
     const headerSection = document.querySelector(".section-header");
     const paginationSection = document.querySelector(".movie-pagination");
+    const searchInput = document.querySelector(".sidebar-search-input");
 
     if (!btn) return;
 
-    // Toggles active class to sidebar and stops the function
+    // Toggles active class to sidebar if expand button is clicked and stops the function
 
     if (btn.dataset.typeBtn === "expand") {
       sidebar.classList.toggle("active");
       this.buttonPage = btn.dataset.page;
+      return;
+    }
+
+    // Expands sidebar when search button is clicked
+    // and the button only works if sidebar is not expanded
+    if (
+      btn.dataset.typeBtn === "form-btn" &&
+      !sidebar.classList.contains("active")
+    ) {
+      sidebar.classList.toggle("active");
+
+      // Focus doesnt work without a little delay
+      setTimeout(() => searchInput.focus(), 100);
+
+      this.buttonPage = btn.dataset.page;
+      return;
+    }
+
+    // When the sidebar is expanded and search btn is clicked the function stops
+    if (btn.dataset.typeBtn === "form-btn") {
       return;
     }
 
@@ -76,7 +98,6 @@ class SideBarBtnView {
       });
 
       // Toggles active class to the buttons
-
       btn.querySelector(".bx").classList.toggle("active");
       btn.classList.toggle("active");
     }
