050d387be388a9467cab4a6630421ddca9aec19a
alicalimli <alicalimli76@gmail.com>
2022-05-16 11:35:46 +0300

    made the image unavailable redirect to google search when clicked

--- src/scripts/views/expansionView.js
+++ src/scripts/views/expansionView.js
@@ -322,10 +322,10 @@ class expansionView {
             </a>
             `
               : `
-            <div class="flx flx-cntr flx-clmn picture-cicle">
+            <a href="https://www.google.com/search?q=${cast.name}" target="_blank" class="flx flx-cntr flx-clmn picture-cicle">
               <i class="ph-icon ph-warning"></i>
               <p class="img-unavailable-text">Image <br> Unavailable</p>
-            </div>
+            </a>
             `
           }
           <figcaption class="cast-name">${cast.name}</figcaption>
